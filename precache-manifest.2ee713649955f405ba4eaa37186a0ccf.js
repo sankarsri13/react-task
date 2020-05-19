56928c052c4227207d8d261aadcd71ec7ec300bd
Shankara Sharma <sankarsri1313@gmail.com>
2020-05-19 12:07:34 +0530

    Updates

---
+++ precache-manifest.2ee713649955f405ba4eaa37186a0ccf.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626a73806b3ab796761d74d7ef4dfa06",
+    "url": "/react-task/index.html"
+  },
+  {
+    "revision": "457188ca9dd509c999f6",
+    "url": "/react-task/static/css/main.809ea659.chunk.css"
+  },
+  {
+    "revision": "ae388b22c5e7e73205c1",
+    "url": "/react-task/static/js/2.0e60c35a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-task/static/js/2.0e60c35a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457188ca9dd509c999f6",
+    "url": "/react-task/static/js/main.799cb30e.chunk.js"
+  },
+  {
+    "revision": "c7970c91f3740b7422e1",
+    "url": "/react-task/static/js/runtime-main.7c3b674a.js"
+  },
+  {
+    "revision": "70bd327b760b6531859c43946e150a70",
+    "url": "/react-task/static/media/1.70bd327b.jpg"
+  },
+  {
+    "revision": "97ae44d0021eb01871c83a40db513169",
+    "url": "/react-task/static/media/11.97ae44d0.jpg"
+  },
+  {
+    "revision": "ca6d57b94d74a225728a2b6ab9ddf1b6",
+    "url": "/react-task/static/media/12.ca6d57b9.jpg"
+  },
+  {
+    "revision": "7705d7b0f4bfd788c0bec3189b603e21",
+    "url": "/react-task/static/media/13.7705d7b0.jpg"
+  },
+  {
+    "revision": "a018b888472a03581d0600a61c156c63",
+    "url": "/react-task/static/media/14.a018b888.jpg"
+  },
+  {
+    "revision": "da4de55587f0b23d0fd77f50f0c3fcea",
+    "url": "/react-task/static/media/15.da4de555.jpg"
+  },
+  {
+    "revision": "c952fdc5c558b4c0de8303929b270319",
+    "url": "/react-task/static/media/2.c952fdc5.jpg"
+  },
+  {
+    "revision": "b5b67f94b3992aba56bdf3cd2613e769",
+    "url": "/react-task/static/media/3.b5b67f94.jpg"
+  },
+  {
+    "revision": "7a128bbc4d60b56d79ff5bfd9a04cf61",
+    "url": "/react-task/static/media/4.7a128bbc.jpg"
+  },
+  {
+    "revision": "89747c2c4da94fd7893b0de32e24e51c",
+    "url": "/react-task/static/media/5.89747c2c.jpg"
+  },
+  {
+    "revision": "829335d229cf2a5270cc3fae11c85e63",
+    "url": "/react-task/static/media/AL2.829335d2.jpg"
+  },
+  {
+    "revision": "0a16fcde0a9d1a3bd181686c517c278f",
+    "url": "/react-task/static/media/Alarab.0a16fcde.jpg"
+  },
+  {
+    "revision": "e86a2bd22af051e225d7342e6e7002cb",
+    "url": "/react-task/static/media/dubai-bg.e86a2bd2.jpg"
+  },
+  {
+    "revision": "3e592e9cbd71b55f78b34c8f4012219e",
+    "url": "/react-task/static/media/mail.3e592e9c.png"
+  },
+  {
+    "revision": "d66e479c3a9979bafc6cdbd053775d2d",
+    "url": "/react-task/static/media/map.d66e479c.png"
+  },
+  {
+    "revision": "f82e0132949d21090308e3a03cf3b731",
+    "url": "/react-task/static/media/path.f82e0132.png"
+  },
+  {
+    "revision": "6ff534f89808fa45fe4924730761276c",
+    "url": "/react-task/static/media/tree2.6ff534f8.jpg"
+  }
+]);
